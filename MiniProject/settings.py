20188fec0c0960043186a7afac712cb3472716e0
Dishant <taredishant21@gmail.com>
2021-08-19 16:37:05 +0530

    set debug=false

--- MiniProject/settings.py
+++ MiniProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'iq*v!=5o7x2t=-8z7*c@!0ysq00v!778wkhc8m)w01*tcwskt6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
